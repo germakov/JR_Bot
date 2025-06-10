b3ba066ff89d79bbabbd3b33e4e095130eddf80f
German <gvermakov@yandex.ru>
2025-06-10 12:24:59 +0300

    add quotes config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TG_BOT_TOKEN = os.getenv(TG_TOKEN)
-CHATGPT_TOKEN = os.getenv(GPT_TOKEN)
+TG_BOT_TOKEN = os.getenv("TG_TOKEN")
+CHATGPT_TOKEN = os.getenv("GPT_TOKEN")
 
-if not all((TG_BOT_TOKEN, CHATGPT_TOKEN)):
+if not all([TG_BOT_TOKEN, CHATGPT_TOKEN]):
     raise ValueError("Введите токены в .env")
